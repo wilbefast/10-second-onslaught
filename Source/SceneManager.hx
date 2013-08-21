b2a604f0b424aa73505ea7bed9a3bd75c2ccbe99
wilbefast <wilbefast@gmail.com>
2013-08-21 22:52:27 +0200

    push/pop scene manager

--- Source/SceneManager.hx
+++ Source/SceneManager.hx
@@ -1,6 +1,8 @@
 import flash.Lib;
 import flash.events.Event;
 import flash.events.MouseEvent;
+import haxe.ds.GenericStack;
+import haxe.ds.StringMap;
 
 class SceneManager
 {
@@ -12,7 +14,7 @@ class SceneManager
 
 	private function new()
 	{ 
-		byName = new Map<String, Scene>();
+		byName = new StringMap<Scene>();
 		// events
 		Lib.current.stage.addEventListener(Event.RESIZE, function(event) { current.onResize(event); } );
 		Lib.current.stage.addEventListener(Event.ENTER_FRAME, function(event) { current.onFrameEnter(event); } );
@@ -31,20 +33,24 @@ class SceneManager
 
 	// shortcut static functions
 
-	public static function setScene(newScene : Scene) : Void
+	public static function setScene(newSceneName : String) : Void
 	{
-		get().__setScene(newScene);
+		get().__setScene(newSceneName);
 	}
-
-	public static function setSceneByName(newSceneName : String) : Void
-	{
-		get().__setSceneByName(newSceneName);
-	}
-
 
 	public static function add(newSceneName : String, newScene : Scene)
 	{
 		return get().__add(newSceneName, newScene);
+	}
+
+	public static function push(newSceneName : String)
+	{
+		return get().__push(newSceneName);
+	}
+
+	public static function pop()
+	{
+		return get().__pop();
 	}
 
 
@@ -54,29 +60,53 @@ class SceneManager
 
 	private var current : Scene;
 
-	private function __setScene(newScene : Scene) : Void
+	private function __setSceneObject(newScene : Scene) : Void
 	{
 		current.onExit(newScene);
 		newScene.onEnter(current);
-
 		current = newScene;
 	}
 
-	private function __setSceneByName(newSceneName : String) : Void
+	private function __setScene(newSceneName : String) : Void
 	{
-		setScene(byName[newSceneName]);
+		if(byName.exists(newSceneName))
+			__setSceneObject(byName.get(newSceneName));
+		else
+			trace("Invalid scene name '" + newSceneName + "' passed to SceneManager::setScene");
 	}
 
 	// ---------------------------------------------------------------------------
 	// SCENES BY NAME
 	// ---------------------------------------------------------------------------
 
-	private var byName : Map<String, Scene>;
+	private var byName : StringMap<Scene> ;
 
 	private function __add(newSceneName : String, newScene : Scene)
 	{
-		byName[newSceneName] = newScene;
+		byName.set(newSceneName, newScene);
 		if(current == null)
 			current = newScene;
+	}
+
+	// ---------------------------------------------------------------------------
+	// SCENES BY STACK OF NAMES
+	// ---------------------------------------------------------------------------
+
+	private var stack : GenericStack<Scene>;
+
+	private function __push(sceneName : String)
+	{
+		if(byName.exists(sceneName))
+			stack.add(byName.get(sceneName));
+		else
+			trace("Invalid scene name '" + sceneName + "' passed to SceneManager::push");
+	}
+
+	private function __pop()
+	{
+		if(stack.isEmpty())
+			trace("Nothing to pop from stack in SceneManager::pop");
+		else
+			__setSceneObject(stack.pop());
 	}
 }
