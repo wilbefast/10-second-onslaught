89f4600fab0de5eb382ca7a73629f669f9c1a5c4
wilbefast <wilbefast@gmail.com>
2013-08-24 03:36:59 +0200

    testing animations

--- Source/Marine.hx
+++ Source/Marine.hx
@@ -1,6 +1,12 @@
 import openfl.Assets;
 import flash.display.Bitmap;
+import flash.geom.Rectangle;
 import flash.display.BitmapData;
+
+import spritesheet.AnimatedSprite;
+import spritesheet.data.BehaviorData;
+import spritesheet.importers.BitmapImporter;
+import spritesheet.Spritesheet;
 
 class PlasmaGun extends UnitWeapon
 {
@@ -19,10 +25,15 @@ class Marine extends Unit
 	private static var initialised : Bool = false;
 
 	private static var bitmapData : BitmapData;
+	private static var sheet : Spritesheet;
 
 	private static function init() : Void
 	{
 		bitmapData = Assets.getBitmapData("assets/marine.png");
+
+		sheet = BitmapImporter.create(Assets.getBitmapData("assets/gore.png"), 5, 1, 48, 48);
+		sheet.addBehavior(new BehaviorData("boom", [0, 1, 2], true, 5));
+
 		initialised = true;
 	}
 
@@ -31,6 +42,7 @@ class Marine extends Unit
 	// ---------------------------------------------------------------------------
 
 	private var bitmap : Bitmap;
+	private var animated : AnimatedSprite;
 
 	private static inline var HITPOINTS : Int = 100;
 	private static inline var RADIUS : Int = 16;
@@ -45,10 +57,14 @@ class Marine extends Unit
 		team = Unit.TEAM_MARINES;
 		weapon = new PlasmaGun();
 
-		bitmap = new Bitmap(bitmapData);
+		/*bitmap = new Bitmap(bitmapData);
 		bitmap.x = -bitmap.width/2;
 		bitmap.y = -bitmap.height*0.75;
-		addChild(bitmap);
+		addChild(bitmap);*/
+
+		animated = new AnimatedSprite(sheet, true);
+		animated.showBehavior("boom");
+		addChild(animated);
 	}
 
 	// ---------------------------------------------------------------------------
@@ -58,6 +74,9 @@ class Marine extends Unit
 	public override function update(dt : Float) : Void
 	{
 		super.update(dt);
+
+		// update animation
+		animated.update(cast(dt*1000, Int));
 
 		// get new target
 		if(target == null || target.purge)
