19de5310e1df7410cc527cfb7c720c181bce2ae0
wilbefast <wilbefast@gmail.com>
2013-08-27 01:00:45 +0200

    timeline at bottom of screen

--- Source/TimelineUI.hx
+++ Source/TimelineUI.hx
@@ -16,8 +16,8 @@ class TimelineUI extends Sprite
 	
 	private var session : Session;
 	private var selection : Sprite;
-	private var bar_bitmap : Bitmap;
-	private var background_bitmap : Bitmap;
+	private var bar : Sprite;
+	private var background : Sprite;
 
 	private var text : DefaultTextField;
 
@@ -43,51 +43,48 @@ class TimelineUI extends Sprite
 		// build draw list
 
 		// ... background
-		background_bitmap = new Bitmap(background_bd);
-		addChild(background_bitmap);
+		background = new Sprite();
+		background.addChild(new Bitmap(background_bd));
+		addChild(background);
 		
 		// ... bar
-		bar_bitmap = new Bitmap(bar_bd);
-		bar_bitmap.alpha = 0.7;
-		bar_bitmap.width = 0;
-		addChild(bar_bitmap);
+		bar = new Sprite();
+		bar.addChild(new Bitmap(bar_bd));
+		bar.alpha = 0.7;
+		bar.width = 0;
+		addChild(bar);
 
 		// ... selection
 		var selection_bitmap = new Bitmap(selectionContainer_bd);
+
 		selection_bitmap.x = -selection_bitmap.width/2;
 		selection = new Sprite();
 		selection.addChild(selection_bitmap);
 		addChild(selection);
-
-		// .. text
-		text = new DefaultTextField("test", width/2, -32);
-		addChild(text);
 	}
 
 	private var slotWidth : Float = 0;
 
 	public function recalculateLayout()
 	{
-		background_bitmap.width = stage.stageWidth;
-		bar_bitmap.x = (background_bd.width / stage.stageWidth)*3;
+		background.width = stage.stageWidth;
+		bar.x = (background.width / stage.stageWidth)*3;
 	}
 	
 	public function update(time : Float)
 	{
-		var slotWidth = background_bitmap.width * 0.1;
-		bar_bitmap.width = selection.x = time * slotWidth;
+		var slotWidth = background.width * 0.1;
+		bar.width = selection.x = time * slotWidth;
 	}
 	
 	private function onMouseClick(event : MouseEvent) : Void
   {
-		var slotWidth = (background_bitmap.width-bar_bitmap.x*2) * 0.1;
+		var slotWidth = (background.width-bar.x*2) * 0.1;
 		session.setTimelineSelection(Math.round(event.stageX / slotWidth));
 
 		var select_x = session.getTimelineSelection() * slotWidth;
 
 		Actuate.tween(selection, 0.3, { x : select_x }, true).ease (Quad.easeOut);
-		Actuate.tween(bar_bitmap, 0.3, { width : select_x }, true).ease (Quad.easeOut);
-
-		text.text = "selecting : " + session.getTimelineSelection();
+		Actuate.tween(bar, 0.3, { width : select_x }, true).ease (Quad.easeOut);
   }
 }
