60aba727fc6d3c10c676120b26a02d51390a470c
Anthony <anthony@naturalpad.fr>
2013-08-27 03:02:12 +0200

    victory screen is alright

--- Source/VictoryScene.hx
+++ Source/VictoryScene.hx
@@ -1,16 +1,36 @@
 import flash.events.MouseEvent;
+import flash.display.BitmapData;
+import flash.display.Bitmap;
+import openfl.Assets;
 
 class VictoryScene extends Scene
 {
 
+	private static var bitmapData : BitmapData; 
+	private static var initialised = false;
+	
 	public function new() 
 	{
-		super ();
-		addChild (new DefaultTextField("Victory", 200, 200));
+		super();
+
+		if(!initialised)
+		{
+			bitmapData = Assets.getBitmapData("assets/vicoty_screen.jpg");
+			initialised = true;
+		}
+		
+		var bitmap = new Bitmap(bitmapData) ;
+		addChild(bitmap);
 	}
 	
 	public override function onMouseClick(event : MouseEvent) : Void
 	{
 		SceneManager.setScene("Title");
+	}
+
+	public override function onEnter(previous : Scene)
+	{
+		width = stage.stageWidth ;
+		height = stage.stageHeight ;
 	}
 }
