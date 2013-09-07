6ed604e5288a28c482fca55685b7b400b264eefd
wilbefast <wilbefast@gmail.com>
2013-09-07 23:29:25 +0200

    added licence header to source files

--- Source/StarWarsTextField.hx
+++ Source/StarWarsTextField.hx
@@ -1,3 +1,18 @@
+/* 
+'10 Second Onslaught' OpenFL game
+(C) Copyright 2013 William Dyce, Antoine Seilles
+
+All rights reserved. This program and the accompanying materials
+are made available under the terms of the GNU Lesser General Public License
+(LGPL) version 3 which accompanies this distribution, and is available at
+http://www.gnu.org/licenses/lgpl.html
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+*/
+
 import flash.text.TextField;
 
 import hacksaw.DefaultTextField;
--- Source/hacksaw/RadialMenu.hx
+++ Source/hacksaw/RadialMenu.hx
@@ -1,3 +1,18 @@
+/* 
+'Hacksaw' game library for Haxe / OpenFL
+(C) Copyright 2013 William Dyce
+
+All rights reserved. This program and the accompanying materials
+are made available under the terms of the GNU Lesser General Public License
+(LGPL) version 3 which accompanies this distribution, and is available at
+http://www.gnu.org/licenses/lgpl.html
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+*/
+
 package hacksaw;
 
 import flash.display.BitmapData;
